970ec5c5a01c5f08c0eb4e4f834af2edbc5d444d
ilyes-guy <ilyes.guy@gmail.com>
2022-12-21 18:16:47 +0100

    dz

--- src/Client.java
+++ src/Client.java
@@ -20,3 +20,40 @@ public class Client extends Application {
         launch(args);
     }
 }
+
+
+
+/*
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+
+
+
+
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter your nick: ");
+        String name = s.nextLine();
+        Socket socket = new Socket("localhost", 5555);
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    while (true)                            
+                        System.out.println(dis.readUTF());
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }).start();
+        dos.writeUTF(name + " has joined the conversation");
+        while (true)        
+                                             
+            dos.writeUTF(name + ": " + s.nextLine());
+    }
+}*/
--- src/Server.java
+++ src/Server.java
@@ -20,3 +20,46 @@ public class Server extends Application {
         launch(args);
     }
 }
+
+
+
+
+
+
+
+
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        ServerSocket server = new ServerSocket(5555);
+        List<DataOutputStream> doss = new ArrayList<>();
+        while (true) {
+            Socket client = server.accept();
+            synchronized(doss) {
+                doss.add(new DataOutputStream(client.getOutputStream()));
+            }
+            new Thread(new Runnable() {
+                public void run() {
+                    try (DataInputStream dis = new DataInputStream(client.getInputStream())) {
+                        while(true){
+                            String message=dis.readUTF();
+                            synchronized (doss) {
+                                for (DataOutputStream dos : doss)
+                                    dos.writeUTF(message);
+                            }
+                        }
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
+}
+
+
