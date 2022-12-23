c34bad5ebd3f0cfe6a5a7e810d60af5880d90895
ilyes-guy <ilyes.guy@gmail.com>
2022-12-23 21:29:06 +0100

    za d

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,10 +1,8 @@
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 
 
 import java.io.*;
@@ -13,37 +11,43 @@ import java.util.*;
 
 
 
+
 public class Server extends Application {
+    static Controller myControllerHandle;
+
     private Socket client;
     private ServerSocket server;
-    private ObjectInputStream dis;
-    List<DataOutputStream> doss ;
-    
-    
-    
+    private ObjectInputStream dis2;
+    private List<DataOutputStream> doss ;
+
+
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("server_ui.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("client_ui.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("server_ui.fxml"));
+        Parent root = loader.load();
+        myControllerHandle = (Controller)loader.getController();
+        
         primaryStage.setTitle("Registration Form FXML Application");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
-    //test test
+    
 
-    public void main(String[] args) throws Exception {
-        System.out.print("//////starting ui////////////// \n");
+
+
+    public static void main(String[] args) throws Exception{
         launch(args);
-        System.out.print("//////starting server////////////// \n");
+        
 
-        this.server = new ServerSocket(5555);
-        this.doss = new ArrayList<>();
+        server = new ServerSocket(5555);
+        doss = new ArrayList<>();
         while (true) {
-            this.client = server.accept();
-            System.out.print("//////user connected////////////// \n");
-            this.dis = new ObjectInputStream(client.getInputStream());
-            final Map<String, String> message_object = (Map)dis.readObject();
-            System.out.print("//////"+message_object.get("method")+"////////////// \n");
-
+            client = server.accept();
+            dis2 = new ObjectInputStream(client.getInputStream());
+            final Map<String, String> message_object = (Map)dis2.readObject();
             if(message_object.get("method") == "start_chat"){
                 start_new_thread();
             }else if(message_object.get("method") == "start_chat"){
@@ -51,6 +55,7 @@ public class Server extends Application {
             }
         }
     }
+
 
 
     public void start_new_thread() throws IOException{
@@ -74,59 +79,16 @@ public class Server extends Application {
                 }
             }).start();
     }
-    
-}
 
-
-
-/*
-            Sql_connection db = new Sql_connection();
-            String[] params = {email.getText(), password.getText()};
-            ResultSet result = db.select_query("SELECT  * from users where email=? and password=?", params);
-            while (result.next()) {
-                System.out.println(result.getString("username"));
-            }
-            db.closeConnection();
-*/
-
-
-/*
-
-
-
-
-import java.io.*;
-import java.net.*;
-import java.util.ArrayList;
-import java.util.List;
-
-
-public class Server {
-    public static void main(String[] args) throws Exception {
-        ServerSocket server = new ServerSocket(5555);
-        List<DataOutputStream> doss = new ArrayList<>();
-        while (true) {
-            Socket client = server.accept();
-            synchronized(doss) {
-                doss.add(new DataOutputStream(client.getOutputStream()));
-            }
-            new Thread(new Runnable() {
-                public void run() {
-                    try (DataInputStream dis = new DataInputStream(client.getInputStream())) {
-                        while(true){
-                            String message=dis.readUTF();
-                            synchronized (doss) {
-                                for (DataOutputStream dos : doss)
-                                    dos.writeUTF(message);
-                            }
-                        }
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            }).start();
-        }
+    public void login() throws IOException{
+        
     }
-}
+    public void register() throws IOException{
+        
+    }
 
-*/
+
+
+
+
+}
