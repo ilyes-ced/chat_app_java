877b79f22b8a167591da3f6622b4baf6659103a9
ilyes-guy <ilyes.guy@gmail.com>
2022-12-23 19:06:48 +0100

    zd

--- login/Login_controller.java
+++ login/Login_controller.java
@@ -1,11 +1,14 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import java.sql.*;
 
 public class Login_controller {
+    @FXML
+    private CheckBox remember_input;
 
     @FXML
     private Button cancel_button;
@@ -29,7 +32,7 @@ public class Login_controller {
     void submit_form(ActionEvent event) {
        try{
             Sql_connection db = new Sql_connection();
-            ResultSet result = db.select_query("SELECT  * from users",);
+            ResultSet result = db.select_query("SELECT  * from users");
             while (result.next()) {
                 System.out.println(result.getString("username"));
             }
