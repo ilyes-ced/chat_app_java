f11a0b9460df5819d9cbccea991541413cf7c73b
ilyes-guy <ilyes.guy@gmail.com>
2023-01-03 22:31:38 +0100

    zef az ddaz d

--- client/src/main/java/org/openjfx/App.java
+++ client/src/main/java/org/openjfx/App.java
@@ -17,7 +17,7 @@ import java.util.Scanner;
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader login_page_loader = new FXMLLoader(App.class.getResource("login.fxml"));
+        FXMLLoader login_page_loader = new FXMLLoader(App.class.getResource("fxml/login.fxml"));
         Parent login_pane = login_page_loader.load();
         Scene login_scene = new Scene(login_pane);
         
